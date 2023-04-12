06eb8991d31cfbddad22c3b572100937fa8eb5eb
LauritsSchou <laurits.schou@gmail.com>
2023-04-12 11:59:53 +0200

    updateData function
    pushes a new number at the end of the array and shifts the rest of the elements

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 window.addEventListener("load", initApp);
 
-const data = [16, 25, 6, 22, 11, 4, 7, 15, 19, 1, 6, 31, 19, 11, 9, 8, 2, 29, 31, 18, 7, 17, 3, 22, 6, 21, 26, 14, 24, 4, 11, 22, 17, 20, 10, 29, 20, 22, 26, 25, 13];
+const data = [16, 25, 6, 22, 11, 4, 7, 15, 19, 1, 6, 31, 19, 11, 9, 8, 2, 29, 31, 18, 7, 17, 3, 22, 6, 21, 26, 14, 24, 4, 11, 22, 17, 20, 10, 29, 20, 22, 26, 25];
 
 function initApp() {
   console.log("JS is running");
@@ -15,4 +15,8 @@ function displayData() {
     const height = (data[i] / 32) * 100;
     currentBar.style.height = `${height}px`;
   }
+}
+function updateData() {
+  data.push(9);
+  data.shift();
 }
